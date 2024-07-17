65d1a2ab3244b7e58a1684bc74626a5eca95fd8e
Nick Clyde <nick@clyde.tech>
2024-07-17 12:48:08 -0700

    Use LFS for seed.sql

--- seed.sql
+++ seed.sql
@@ -1,33 +1,3 @@
--- Create a `snippets` table.
-CREATE TABLE snippets (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
-    content TEXT NOT NULL,
-    created TIMESTAMP NOT NULL,
-    expires TIMESTAMP NOT NULL
-);
-
--- Add an index on the created column.
-CREATE INDEX idx_snippets_created ON snippets(created);
-
--- Add some dummy records (which we'll use in the next couple of chapters).
-INSERT INTO snippets (title, content, created, expires) VALUES (
-    'An old silent pond',
-    'An old silent pond...\nA frog jumps into the pond,\nsplash! Silence again.\n\n– Matsuo Bashō',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP + INTERVAL '365 days'
-);
-
-INSERT INTO snippets (title, content, created, expires) VALUES (
-    'Over the wintry forest',
-    'Over the wintry\nforest, winds howl in rage\nwith no leaves to blow.\n\n– Natsume Soseki',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP + INTERVAL '365 days'
-);
-
-INSERT INTO snippets (title, content, created, expires) VALUES (
-    'First autumn morning',
-    'First autumn morning\nthe mirror I stare into\nshows my father''s face.\n\n– Murakami Kijo',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP + INTERVAL '7 days'
-);
+version https://git-lfs.github.com/spec/v1
+oid sha256:175ccf20b92f34e47944f38ce438acc4d2b9ec62335b92f5459f8c7683efbcc7
+size 1170
